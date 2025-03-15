ccf909427488d2d1051c7e1f5e171b0b955a9f92
JosebaEceiza <j.eceiza@opendeusto.es>
2025-03-15 12:08:46 +0100

    Initial commit

---
+++ src/main/java/es/deusto/spq/doctorclick/DoctorClickApplication.java
@@ -0,0 +1,13 @@
+package es.deusto.spq.doctorclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorClickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorClickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/deusto/spq/doctorclick/DoctorClickApplicationTests.java
@@ -0,0 +1,13 @@
+package es.deusto.spq.doctorclick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorClickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
