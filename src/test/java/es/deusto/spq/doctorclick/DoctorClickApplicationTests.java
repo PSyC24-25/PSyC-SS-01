3c1a35680dd0740ba6c48ad983820493eaae084d
Iker Aramendi Ugalde <iker.aramendi@opendeusto.es>
2025-04-24 10:44:36 +0200

    Añadido un test de pacienteService y corregido el pom.xml

--- src/test/java/es/deusto/spq/doctorclick/DoctorClickApplicationTests.java
+++ src/test/java/es/deusto/spq/doctorclick/DoctorClickApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DoctorClickApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
--- src/test/java/es/deusto/spq/doctorclick/service/PacienteServiceTest.java
+++ src/test/java/es/deusto/spq/doctorclick/service/PacienteServiceTest.java
@@ -1,4 +1,45 @@
 package es.deusto.spq.doctorclick.service;
 
+import es.deusto.spq.doctorclick.model.Paciente;
+import es.deusto.spq.doctorclick.repository.PacienteRepository;
+import es.deusto.spq.doctorclick.service.PacienteService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
 public class PacienteServiceTest {
+    @Mock
+    PacienteRepository pacienteRepository;
+
+    @InjectMocks
+    PacienteService pacienteService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this); // Activa las anotaciones @Mock y @InjectMocks
+    }
+
+    @Test
+    @DisplayName("Registrar paciente nuevo correctamenete")
+    void registrarPaciente() {
+        Paciente paciente = new Paciente("97147611G","Sara","García","pass123");
+
+        //Simular que no existe el paciente
+        when(pacienteRepository.findByDni("12345678A")).thenReturn(Optional.empty());
+
+        boolean resultado = pacienteService.registrarPaciente(paciente);
+        assertTrue(resultado);
+        verify(pacienteRepository).save(paciente);
+
+    }
+
+
 }
