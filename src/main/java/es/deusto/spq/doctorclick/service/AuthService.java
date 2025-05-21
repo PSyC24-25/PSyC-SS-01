24b821d592ce77fab0d52e79c71c9eae20ed9383
keelus <hugomoreda@hotmail.com>
2025-05-21 11:13:33 +0200

    Documentar funciones de AuthService.

--- src/main/java/es/deusto/spq/doctorclick/service/AuthService.java
+++ src/main/java/es/deusto/spq/doctorclick/service/AuthService.java
@@ -7,10 +7,17 @@ import com.nimbusds.jwt.*;
 import java.text.ParseException;
 import java.util.Date;
 
+/**
+ * Servicio utilitario encargado de crear , validar y extraer datos de los tokens JWT usados
+ * para las sesiones.
+ */
 public class AuthService {
-    // La llave es de prueba, esto deberia de ir en un .env
     private static final byte[] PRIVATE_KEY = "f9d7776651394fdae1b70e819f8149c51c52d49e19962ee6cb6b193ce7f6f18a".getBytes();
 
+	/**
+	 * CrearTokenJWT crea un token JWT a partir del DNI y el tipo de usuario ('medico', 'paciente').
+	 * Devuelve el token como String, firmado con la llave privada.
+	 */
     public static String CrearTokenJWT(String dni, String tipo) throws JOSEException {
         JWSSigner signer = new MACSigner(PRIVATE_KEY);
 
@@ -26,6 +33,10 @@ public class AuthService {
         return signedJWT.serialize();
     }
 
+	/**
+	 * EsJWTValido devolvera `true` si el token JWT pasado como parametro es valido/firmado por nosotros.
+	 * En cualquier otro caso, ya sea JWT caducado, checksum no valido, o cualquier otro error, devolvera `false`.
+	 */
     public static boolean EsJWTValido(String tokenJwt) throws JOSEException, ParseException {
         if(tokenJwt == null) return false;
 
@@ -47,6 +58,9 @@ public class AuthService {
         return true;
     }
 
+	/**
+	 * ObtenerClaimsJWT devuelve un mapa (`JWTClaimsSet`) con los datos embebidos en el JWT.
+	 */
     public static JWTClaimsSet ObtenerClaimsJWT(String tokenJwt) throws ParseException {
         SignedJWT signedJWT = SignedJWT.parse(tokenJwt);
         JWTClaimsSet claims = signedJWT.getJWTClaimsSet();
